3e643d24c89cbe8d20db8385fb1dab38ba769708
shreyaa1 <shuklashreya801@gmail.com>
2020-05-04 00:00:54 +0530

    added node server and polyfill for bind

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("Node is working");
+
+//server.js
+
+const http = require("http"),
+  server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello world");
+    response.end();
+  });
+server.listen(3000, () => {
+  console.log("Node server created at port 3000");
+});
